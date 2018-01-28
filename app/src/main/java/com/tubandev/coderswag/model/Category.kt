1a066808e34bdc2a7f9850ab5987b008332c6d8c
sulistiyanto <sulistiyanto404@gmail.com>
2018-01-28 11:00:39 +0700

    lesson 2

--- app/src/main/java/com/tubandev/coderswag/model/Category.kt
+++ app/src/main/java/com/tubandev/coderswag/model/Category.kt
@@ -3,4 +3,9 @@ package com.tubandev.coderswag.model
 /**
  * Created by sulistiyanto on 28/01/18.
  */
-class Category(val title : String, val image : String)
+class Category(val title : String, val image : String) {
+
+    override fun toString(): String {
+        return title
+    }
+}
