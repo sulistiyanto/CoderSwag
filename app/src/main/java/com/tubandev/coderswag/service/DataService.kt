8f09daab8f23144073e7c3d6b94a1d1401a29635
sulistiyanto <sulistiyanto404@gmail.com>
2018-01-28 20:51:58 +0700

    lesson 07

--- app/src/main/java/com/tubandev/coderswag/service/DataService.kt
+++ app/src/main/java/com/tubandev/coderswag/service/DataService.kt
@@ -23,25 +23,37 @@ object DataService {
             Category("DIGITAL", "digitalgoodsimage")
     )
 
-    val hats = listOf(
-            Product("TubanDev Graphic Beanie", "$18", "hat01"),
-            Product("TubanDev Hat Black", "$20", "hat02"),
-            Product("TubanDev Hat White", "$18", "hat03"),
-            Product("TubanDev Hat Snapback", "$22", "hat04")
+    private val hats = listOf(
+            Product("TubanDev Graphic Beanie", "$18", "hat1"),
+            Product("TubanDev Hat Black", "$20", "hat2"),
+            Product("TubanDev Hat White", "$18", "hat3"),
+            Product("TubanDev Hat Snapback", "$22", "hat4")
     )
 
-    val hoodies = listOf(
-            Product("TubanDev Hoodie Gray", "$28", "hoodie01"),
-            Product("TubanDev Hoodie Red", "$32", "hoodie02"),
-            Product("TubanDev Hoodie Black", "$38", "hoodie03"),
-            Product("TubanDev Hoodie Blue", "$22", "hoodie04")
+    private val hoodies = listOf(
+            Product("TubanDev Hoodie Gray", "$28", "hoodie1"),
+            Product("TubanDev Hoodie Red", "$32", "hoodie2"),
+            Product("TubanDev Hoodie Black", "$38", "hoodie3"),
+            Product("TubanDev Hoodie Blue", "$22", "hoodie4")
     )
 
-    val shirts = listOf(
-            Product("TubanDev Shirt Gray", "$18", "shirt01"),
-            Product("TubanDev Shirt Red", "$16", "shirt02"),
-            Product("TubanDev Shirt Black", "$18", "shirt03"),
-            Product("TubanDev Shirt Blue", "$22", "shirt04"),
-            Product("Kickflip Studios", "$12", "shirt05")
+    private val shirts = listOf(
+            Product("TubanDev Shirt Gray", "$18", "shirt1"),
+            Product("TubanDev Shirt Red", "$16", "shirt2"),
+            Product("TubanDev Shirt Black", "$18", "shirt3"),
+            Product("TubanDev Shirt Blue", "$22", "shirt4"),
+            Product("Kickflip Studios", "$12", "shirt5")
     )
+
+    private val digitalGood = listOf<Product>()
+
+    fun getProduct(category: String) : List<Product> {
+
+        return when(category) {
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
+        }
+    }
 }
